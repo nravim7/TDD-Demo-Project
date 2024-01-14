f0ce3b2360ca3031e638b1a5e9e9c5a1a15b0d34
Ravi N <your.email@example.com>
2024-01-14 22:34:25 +0530

    Refactoring as both the test case's Arrange and Assert are same

--- src/test/java/org/ravi/estore/service/UserServiceTest.java
+++ src/test/java/org/ravi/estore/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package org.ravi.estore.service;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.ravi.estore.model.User;
 
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UserServiceTest {
 
+    @DisplayName("User object created")
     @Test
     void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
         //Arrange
@@ -24,23 +26,8 @@ public class UserServiceTest {
 
         //Assert
         assertNotNull(user, "The createUser() should not have returned null");
+        assertEquals(firstName, user.getFirstName(),"User's first name is incorrect");
 
     }
 
-    @Test
-    void testCreateUser_whenUserCreated_returnedUserObjectContainsSameFirstName() {
-        //Arrange
-        UserService userService = new UserServiceImpl();
-        String firstName = "Ravi";
-        String lastName = "Nagaraju";
-        String email = "test@gmail.com";
-        String password = "12345";
-        String repeatPassword = "12345";
-
-        //Act
-        User user = userService.createUser(firstName,lastName, email,password, repeatPassword);
-
-        //Assert
-        assertEquals(firstName, user.getFirstName());
-    }
 }
